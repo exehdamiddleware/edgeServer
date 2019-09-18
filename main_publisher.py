b7ee30af6d4ffb72d433ffda3410974fe45dcfb5
Leonardojoao01 <leonardojoao01@hotmail.com>
2019-09-18 08:52:27 -0300

    Foi separado as funcoes de pub e sub, criando arquivos separados para a execução para cada um

---
+++ main_publisher.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import threading
+import _thread
+from subscriber import *
+from publisher import *
+import time
+import json
+
+# https://www.w3schools.com/python/python_json.asp
+
+# Para gerar uuid, entre no link: https://www.uuidgenerator.net
+# O tópico de publicação é o mesmo uuid, dessa forma garanto que dado sensor é publicado num único local 
+sensor_uuid = "3aa027bd-4afc-461c-b353-c2535008f4ce"
+gw_uuid = "3aa027bd-4afc-461c-b353-c2535008f4ce"
+
+# JSON é composto pelos dados da borda, ip e port, e uuid do sensor
+msg = {'uuid': sensor_uuid}
+# convert into JSON:
+msg = json.dumps(msg)
+
+# Dados do gateway
+topic = "GW_"+gw_uuid
+
+pub = Publisher("127.0.0.1", 1883)
+# conta =0
+# while(conta < 10):
+pub.on_publish(msg, topic)
+# conta = conta +1
